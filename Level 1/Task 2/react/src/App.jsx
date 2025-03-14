5364d0ae13e79cc4e33fa52515a7942d199336e1
Swathi S <164871475+swathi131204@users.noreply.github.com>
2025-03-14 15:51:03 +0530

    Add files via upload

---
+++ Level 1/Task 2/react/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  const username = "Swathi";
+  return <h2>Hello, {username}</h2>
+};
+
+export default App;
---
+++ Level 1/Task 3/react/src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    
+     
+      <div> <h3 style={{ color: "red" }}>This IS Me SWATHI</h3>
+        <p>Im Student Of SNS college of Enginnering Studing B.Tech Information Technology </p>
+
+        
+          <h3>About Me</h3>
+          <p>Im From THENI and Im completed My schooling at Santiniketan Mat. High. Sec School.</p>
+        
+
+        
+        </div>
+    
+    
+  );
+};
+
+export default App;
